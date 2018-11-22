75d7f22cc7f6c5f1188bb5e55154e77b72128b3f
giorgiobraz <giorgiobraz@gmail.com>
2018-11-22 11:33:09 -0200

    add arquivos da APS

---
+++ APS/TAD leitor de arquivos/leitorString.h
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct{
+    char* str;
+    char delimitadores[11];
+    int tamStr;
+    int pos;
+}LeitorString;
+
+//PROTOTIPOS
+LeitorString* leitorString_criar(char* str, char delimitador);
+void leitorString_desalocar(LeitorString* leitor);
+int leitorString_ler(LeitorString* leitor, char* endereco);
+int leitorString_temMais(LeitorString* leitor);
+void leitorString_adicionaDelimitador(LeitorString* l, char delimitador);
+
+//IMPLEMENTACAO
