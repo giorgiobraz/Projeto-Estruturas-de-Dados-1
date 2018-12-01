1f563b460ee470ef2a9a1dcbd59ceb88bffba639
MariaPinguelli <mariaferpinguelli@hotmail.com>
2018-12-01 12:07:22 -0200

    Funções para desalocar o arquivo

--- APS/TAD leitor de arquivos/leitorArquivo.h
+++ APS/TAD leitor de arquivos/leitorArquivo.h
@@ -27,6 +27,19 @@ LeitorArquivo* leitorArquivo_criar(char* arquivo, int tamBuffer);
 void leitorArquivo_desalocar(LeitorArquivo* leitor);
 int leitorArquivo_ler(LeitorArquivo* leitor, char* endereco);
 int leitorArquivo_temMaisLinhas(LeitorArquivo* leitor);
-
+//MARIA: "Fiz uma função para desalocar o Scanner, mas não sei se é realmente necessário, se quiser fiquem a vontade para exclui-lo"
+void FileScanner_desalocar(FileScanner* scanner);
 
 //IMPLEMENTACAO
+void leitorArquivo_desalocar(LeitorArquivo* leitor){
+    free(leitor->arquivo);
+    free(leitor->nomeArquivo);
+    free(leitor->buffer);
+    free(leitor);
+}
+
+void FileScanner_desalocar(FileScanner* scanner){
+    free(scanner->arquivo);
+    free(scanner->buffer);
+    free(scanner);
+}
